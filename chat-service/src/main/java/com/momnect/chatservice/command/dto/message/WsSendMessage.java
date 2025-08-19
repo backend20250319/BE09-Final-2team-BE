b795191109e8a9c5debfa6c8290740e8c70876e4
jungho <kiik52@naver.com>
2025-08-19 09:32:52 +0900

    - 웹 소켓 설정파일 추가 및 기본 설정

---
+++ chat-service/src/main/java/com/momnect/chatservice/command/dto/message/WsSendMessage.java
@@ -0,0 +1,10 @@
+package com.momnect.chatservice.command.dto.message;
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class WsSendMessage {
+    private Long roomId;
+    private Long senderId;
+    private String message;
+}
