788aa0bdb0a1d0f5c47e0ab8cae349241cefc2de
BEOMSEOK <abcdef980530@gmail.com>
2025-08-22 15:00:43 +0900

    feat:댓글/좋아요 기능추가

--- post-service/src/main/java/com/momnect/postservice/command/entity/PostLikeId.java
+++ post-service/src/main/java/com/momnect/postservice/command/entity/PostLikeId.java
@@ -1,4 +1,20 @@
 package com.momnect.postservice.command.entity;
 
-public class PostLikeId {
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class PostLikeId implements Serializable {
+    @Column(name = "post_id", nullable = false)
+    private Long postId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
 }
