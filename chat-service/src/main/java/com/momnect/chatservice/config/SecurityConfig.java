306645e09fcc3637122f457503f77190531db96d
jungho <kiik52@naver.com>
2025-08-22 09:13:35 +0900

    feat: radis 설치 및 설정 파일 생성

--- chat-service/src/main/java/com/momnect/chatservice/config/SecurityConfig.java
+++ chat-service/src/main/java/com/momnect/chatservice/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -36,15 +37,25 @@ public class SecurityConfig {
                                 exception.accessDeniedHandler(restAccessDeniedHandler)
                                         .authenticationEntryPoint(restAuthenticationEntryPoint)
                 )
-                .authorizeHttpRequests(auth ->
-                        auth.requestMatchers(HttpMethod.POST, "/auth/signup", "/auth/login")
-                                .permitAll()
-                                .requestMatchers(HttpMethod.GET, "/swagger-ui/**", "/v3/api-docs/**",
-                                        "/swagger-resources/**")
-                                .permitAll()
-                                .anyRequest()
-                                .authenticated()
+                .authorizeHttpRequests(auth -> auth
+                                // 프리플라이트
+                                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+
+                                // swagger, auth 공개
+                                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/swagger-resources/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/auth/signup", "/auth/login").permitAll()
+
+                                // ping, websocket 공개
+                                .requestMatchers("/ping", "/ws/**").permitAll()
+
+                                // ★ 실제 API 전체 공개 (dev 용)
+                                .requestMatchers("/**").permitAll()
+
+                                // 나머지
+//                        .anyRequest().authenticated()
+                                .anyRequest().permitAll()
                 )
+
                 .addFilterBefore(headerAuthenticationFilter(),
                         UsernamePasswordAuthenticationFilter.class)
         ;
